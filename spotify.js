d3e4569de2630c21446380657c350f7749828de6
Shu9696gup <shu9696gup@gmail.com>
2021-11-03 22:36:50 +0530

    asdf

--- spotify.js
+++ spotify.js
@@ -5,16 +5,29 @@ let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById('masterPlay');
 let progressBar=document.getElementById('progressBar');
 let gif= document.getElementsByClassName('gifPlay')[0];
+let songItems= Array.from(document.getElementsByClassName('songItem'));
 
 
-let sons = [
-    { songName: "Let us Love You", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Let us Love You", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
-    { songName: "Let us Love You", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
-    { songName: "Let us Love You", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
-    { songName: "Let us Love You", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" }
+let songs = [
+    { songName: "Let us Love You1", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" ,duration:"3:15", gif: "./playing.gif"},
+
+    { songName: "Let us Love You2", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" ,duration:"2:17",gif:"./playing.gif"},
+    { songName: "Let us Love You3", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" ,duration:"3:50",gif:"./playing.gif"},
+    { songName: "Let us Love You4", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" ,duration:"2:35",gif:"./playing.gif"},
+    { songName: "Let us Love You5", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" ,duration:"3:30",gif:"./playing.gif"},
+    { songName: "Let us Love You6", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" ,duration:"3:15",gif:"./playing.gif"},
+    { songName: "Let us Love You7", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" ,duration:"2:15",gif:"./playing.gif"},
+    { songName: "Let us Love You8", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" ,duration:"2:50",gif:"./playing.gif"},
+    { songName: "Let us Love You9", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" ,duration:"4:15",gif:"./playing.gif"},
+    { songName: "Let us Love You10", filePath: "songs/10.mp3", coverPath: "covers/10.jpg" ,duration:"3:25",gif:"./playing.gif"}
 ]
-
+songItems.forEach((element,i)=>{
+    console.log(element,i);
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+    element.getElementsByClassName("time")[0].innerText=songs[i].duration;
+    element.getElementsByClassName("gifPlay")[0].src=songs[i].gif;
+})
 
 
 
